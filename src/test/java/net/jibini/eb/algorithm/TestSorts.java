cbde0599ef462cef2fb7002bbdd011f6a54d8455
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2020-12-11 14:51:45 -0600

    More strict testing to ensure no elements are changed

--- src/test/java/net/jibini/eb/algorithm/TestSorts.java
+++ src/test/java/net/jibini/eb/algorithm/TestSorts.java
@@ -2,9 +2,11 @@ package net.jibini.eb.algorithm;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Random; 
 
 import org.junit.Assert;
@@ -14,7 +16,7 @@ public class TestSorts
 {
 	private Random random = new Random();
 	
-	private int numElements = 254095;
+	private int numElements = 60095;
 	private int numIterations = 16;
 	
 	private void fillTestValues(List<Integer> elements, int numElements)
@@ -37,17 +39,32 @@ public class TestSorts
 	
 	private void sortAndAssert(List<Integer> elements, Sort<Integer> sort, int numElements)
 	{
+		// Count the number of instances of each number before
+		Map<Integer, Integer> numberCounts = new HashMap<>();
+		for (int i : elements)
+			numberCounts.put(i, numberCounts.getOrDefault(i, 0) + 1);
+		
 		sort.sort(elements);
 		
-		double last = -1.0;
+		// Count the number of instances of each number after
+		for (int i : elements)
+			numberCounts.put(i, numberCounts.getOrDefault(i, 0) - 1);
+		// Ensure the before and after counts are equal
+		for (int i : numberCounts.values())
+			Assert.assertEquals("The count of output values are not equal to the input", 0, i);
 		
+		int last = -1;
+		
+		// Ensure the overall size of the array is equal
 		Assert.assertEquals("Mismatch in list size; elements added or lost", numElements, elements.size());
 		
-		for (double e : elements)
+		for (int i : elements)
 		{
-			Assert.assertTrue("Sort failed; elements are out of order", e >= last);
+			// Ensure each element is greater than (or equal to) the element
+			// before it
+			Assert.assertTrue("Sort failed; elements are out of order", i >= last);
 			
-			last = e;
+			last = i;
 		}
 	}
 	
@@ -143,7 +160,7 @@ public class TestSorts
 	public void canHybridQuickSortArray()
 	{
 		List<Integer> list = new ArrayList<>(numElements);
-		Sort<Integer> sort = new QuickSort<>(comparator, 16, new MergeSort<>(comparator));
+		Sort<Integer> sort = new QuickSort<>(comparator, 32, new HeapSort<>(comparator));
 		
 		templateTestSort(list, sort);
 	}
@@ -152,7 +169,7 @@ public class TestSorts
 	public void canHybridQuickSortLinked()
 	{
 		List<Integer> list = new LinkedList<>();
-		Sort<Integer> sort = new QuickSort<>(comparator, 16, new MergeSort<>(comparator));
+		Sort<Integer> sort = new QuickSort<>(comparator, 32, new HeapSort<>(comparator));
 		
 		templateTestSort(list, sort);
 	}
