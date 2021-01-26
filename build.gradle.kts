50ee815f7591c69593dc2d08120df35b5f37fc15
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2021-01-26 11:20:46 -0600

    Continuous integration config files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.4.21"
+	kotlin("jvm") version "1.4.20"
 
-	java
+	`java-library`
 	application
 
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	id("org.springframework.boot") version "2.4.2"
+
+	jacoco
+	
+	id("com.github.kt3k.coveralls") version "2.10.2"
 }
 
 java {
@@ -61,4 +65,19 @@ dependencies {
 
 	// JUnit 4 test implementation library
 	testImplementation("junit", "junit", "4.12")
+}
+
+tasks.withType<JacocoReport> {
+	classDirectories.setFrom(
+		sourceSets.main.get().output.asFileTree
+	)
+}
+
+coveralls.sourceDirs.addAll(sourceSets.main.get().allSource.srcDirs.map { it.path })
+coveralls.jacocoReportPath = "${buildDir}/reports/jacoco/jacocoRootReport/jacocoRootReport.xml"
+
+tasks.withType<org.kt3k.gradle.plugin.coveralls.CoverallsTask>
+{
+	dependsOn.add(tasks.getByName("jacocoTestReport"))
+	onlyIf { System.getenv()["CI"] != null }
 }
