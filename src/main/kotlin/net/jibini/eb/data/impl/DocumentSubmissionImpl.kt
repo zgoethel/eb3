b82f68ea566a0dfd16498421dd644c4724939db1
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2021-06-29 21:37:18 -0500

    Moving towards bulk document uploads

--- src/main/kotlin/net/jibini/eb/data/impl/DocumentSubmissionImpl.kt
+++ src/main/kotlin/net/jibini/eb/data/impl/DocumentSubmissionImpl.kt
@@ -1,14 +1,18 @@
 package net.jibini.eb.data.impl
 
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
 import net.jibini.eb.EasyButton
 import net.jibini.eb.data.Document
 import net.jibini.eb.data.DocumentDescriptor
 import net.jibini.eb.teststand.impl.StoreFile
 
+import org.slf4j.LoggerFactory
+
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -23,6 +27,8 @@ import java.lang.RuntimeException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpSession
+
+import kotlin.concurrent.thread
 
 /**
  * REST API service for submitting cache contents and cached documents. Allows
@@ -42,14 +48,14 @@ class DocumentSubmissionImpl
     private lateinit var easyButton: EasyButton
 
     @PostMapping("/document/{repository}")
-    fun putDocument(
+    fun putDocuments(
         request: HttpServletRequest,
         response: HttpServletResponse,
 
         session: HttpSession,
 
         @PathVariable(name = "repository") repository: String,
-        @RequestBody documentMap: Map<String, Any?>
+        @RequestBody documents: Array<Map<String, Any?>>
     )
     {
         if (request.getHeader("secret") != easyButton.config.secret)
@@ -63,12 +69,17 @@ class DocumentSubmissionImpl
             throw RuntimeException("Failed to fetch requested descriptor", ex)
         }
 
-        val document = Document(descriptor)
-        document.internal.putAll(documentMap)
-        DocumentRepositoryCachesImpl.put(document)
-
         val storeFile = StoreFile(descriptor, File(".stores/$repository.bin"))
-        storeFile.write(document)
+
+        for (documentMap in documents)
+        {
+            val document = Document(descriptor)
+            document.internal.putAll(documentMap)
+            DocumentRepositoryCachesImpl.put(document)
+
+            storeFile.write(document)
+        }
+
         storeFile.close()
     }
 
@@ -82,14 +93,63 @@ class DocumentSubmissionImpl
             DocumentRepositoryCachesImpl.put(document)
     }
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    private val sendBuffers = mutableMapOf<String, MutableList<Document>>()
+    private val bufferMutex = Mutex(locked = false)
+
+    init
+    {
+        thread(isDaemon = true, name = "Bulk sender")
+        {
+            log.info("Transfer thread is active")
+
+            while (true)
+            {
+                runBlocking {
+                    bufferMutex.withLock {
+                        for ((address, sendBuffer) in sendBuffers)
+                        {
+                            if (sendBuffer.size > 0)
+                                log.info("Transfer of {} documents to central database", sendBuffer.size)
+
+                            try
+                            {
+                                doSend(address, sendBuffer)
+                            } catch (ex: Exception)
+                            {
+                                log.error("Failed to send documents", ex)
+                            }
+
+                            sendBuffer.clear()
+                        }
+
+                    }
+                }
+
+                Thread.sleep(200);
+            }
+        }
+    }
+
+    private fun doSend(address: String, documents: List<Document>)
+    {
+            val rest = RestTemplate()
+//            val headers = HttpHeaders()
+//            headers["secret"] = easyButton.config.secret
+//            headers.contentType = MediaType.APPLICATION_JSON
+
+//            val request = HttpEntity(documents, headers)
+            rest.postForObject(address, documents, ResponseEntity::class.java)
+    }
+
     fun sendDocument(address: String, document: Document)
     {
-        val rest = RestTemplate()
-        val headers = HttpHeaders()
-        headers["secret"] = easyButton.config.secret
-        headers.contentType = MediaType.APPLICATION_JSON
-
-        val request = HttpEntity(document, headers)
-        rest.postForObject(address, request, String::class.java)
+        runBlocking {
+            bufferMutex.withLock {
+                val properSend = sendBuffers.putIfAbsent(address, mutableListOf())!!
+                properSend += document
+            }
+        }
     }
 }
